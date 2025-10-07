93837036d42cd33ff410ccd78b35523195f30925
Gabbyskii <gkadiga1@gmail.com>
2025-10-07 19:08:16 +0200

    textui added

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,10 @@
 import util.java.Arraylist;
 
 class Game {
- //public static void main(String[] args){
-static Arraylist<Player> players = new Arraylist<Player>();
+ public static void main(String[] args){
+ Arraylist<Player> players = new Arraylist<Player>();
 
- public void init(){
+ void init(){
 
   Player p = new Player("Gabby");
   Player p1 = new Player("Melissa");
@@ -12,7 +12,7 @@ static Arraylist<Player> players = new Arraylist<Player>();
   Player p3 = new Player("Zainab");
 
   players.add(p);
-  players.add(p1); 
+  players.add(p1);
   players.add(p2);
   players.add(p3);
 
@@ -23,15 +23,12 @@ static Arraylist<Player> players = new Arraylist<Player>();
   p1.setName("Elev");
  }
 
-private void ListPlayers(){
+ void ListPlayers(){
 
 for(Player p: players){
  System.out.print(p); //player.username+ ": "+player.score)
      System.out.println(p.toString());
    }
  }
- 
-}
-
 
 
